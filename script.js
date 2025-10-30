61579e146e24c53e32c1ec83963fb375870ef828
miyahope <supergirlmiya@gmail.com>
2025-10-30 18:40:54 +0000

    step 2 complete

--- script.js
+++ script.js
@@ -14,21 +14,36 @@ oscillator.type = "sine";
 oscillator.start();
 gainNode.gain.value = 0;
 
+
+const notenames = new Map();
+
+notenames.set("C", 261.6);
+notenames.set("D", 293.7);
+notenames.set("E", 329.6);
+notenames.set("F", 349.2);
+notenames.set("G", 392.0);
+notenames.set("A", 440.0);
+notenames.set("Bb", 466.2);
+notenames.set("Eb", 311.1);
+notenames.set("Ab", 415.3);
+notenames.set("Db", 277.2);
+notenames.set("Gb", 369.9);
+notenames.set("Cb", 246.9);
+
+
+
 function frequency(pitch) {
   gainNode.gain.setValueAtTime(1, audioCtx.currentTime);
   oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
   gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
-  console.log("Playing tone:", pitch);
 }
 
 function handle() {
   audioCtx.resume();
   gainNode.gain.value = 0;
 
-  const pitch = parseFloat(input.value);
-  if (!isNaN(pitch)) {
-    frequency(pitch);
-  } else {
-    alert("Please enter a valid number for frequency!");
-  }
+  var usernotes = String(input.value);
+  frequency(notenames.get(usernotes)); 
+
 }
+
