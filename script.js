9322e180b1a6671923a9b8467977c13121921798
miyahope <supergirlmiya@gmail.com>
2025-10-30 21:08:54 +0000

    step 3

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const input = document.getElementById('input');
 
+//variables (why they so goofyy)
+var pitch;
+var freq = 0;
+var amplitude = 40;
+var interval = null;
+
 // create web audio api elements
 const audioCtx = new AudioContext();
 const gainNode = audioCtx.createGain();
@@ -14,6 +20,45 @@ oscillator.type = "sine";
 oscillator.start();
 gainNode.gain.value = 0;
 
+//define canvas variables
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var width = ctx.canvas.width;
+var height = ctx.canvas.height;
+
+// draw function
+var counter = 0;
+
+function drawWave() {
+  ctx.clearRect(0, 0, width, height);
+  x = 0;
+  y = height/2;
+  ctx.moveTo(x,y);
+  ctx.beginPath();
+
+  counter = 0;
+
+  interval = setInterval (line, 20);
+  
+}
+
+
+
+//sin function
+function line() {
+y = height/2 + (amplitude * Math.sin(x * 2 * Math.PI * freq));
+
+ctx.lineTo(x,y);
+ctx.stroke();
+
+x = x+1;
+counter++;
+
+if(counter > 50) {
+    clearInterval (interval);
+  }
+
+}
 
 const notenames = new Map();
 
@@ -33,6 +78,7 @@ notenames.set("Cb", 246.9);
 
 
 function frequency(pitch) {
+  freq = pitch /10000;
   gainNode.gain.setValueAtTime(1, audioCtx.currentTime);
   oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
   gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
@@ -43,7 +89,7 @@ function handle() {
   gainNode.gain.value = 0;
 
   var usernotes = String(input.value);
-  frequency(notenames.get(usernotes)); 
-
+  pitch = notenames.get(usernotes); 
+  frequency(pitch);
+  drawWave();
 }
-
